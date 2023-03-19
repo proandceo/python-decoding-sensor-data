62c72fee444598d2cb8ceddc6b525fbd1bc4efe6
Rex <proandceo@gmail.com>
2023-03-19 16:51:26 +1000

    Update temperature_info.py

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -3,9 +3,6 @@ from datetime import date, datetime
 
 
 class TemperatureData(HouseInfo):
-    def __init__(self, data):
-        self.data = data
-
     def _convert_data(self, data):
         recs = []
         for rec in data:
@@ -13,11 +10,11 @@ class TemperatureData(HouseInfo):
         return recs
 
     def get_data_by_area(self, rec_area=0):
-        recs = super.get_data_by_area("temperature", rec_area)
+        recs = super().get_data_by_area("temperature", rec_area)
 
         return self._convert_data(recs)
 
     def get_data_by_date(self, rec_date=date.today()):
-        recs = super.get_data_by_date("temperature", rec_date)
+        recs = super().get_data_by_date("temperature", rec_date)
 
         return self._convert_data(recs)
